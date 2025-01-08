acb1f05936128e595e43b2156a6e39820764d58a
Sriram Thiagarajan <sri.sjc@gmail.com>
2025-01-08 08:39:49 +0530

    display data from layout file in page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+	let { data }: { data: LayoutData } = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<p>Data from layout.server.ts: {data.foo}</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
