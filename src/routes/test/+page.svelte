c4a7302436378f39dd2e0793f391f4e91a56db70
Sriram Thiagarajan <sri.sjc@gmail.com>
2025-01-08 21:03:17 +0530

    layout data in nested route

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import type { LayoutData } from '../$types';
+	let { data }: { data: LayoutData } = $props();
+</script>
+
+<h1>Test page</h1>
+<p>Data from layout.server.ts: {data.foo}</p>
